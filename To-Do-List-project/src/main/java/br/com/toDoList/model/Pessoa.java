7277126070088e352b5949fc4cdf76509391a2bc
jhona <jhona@DESKTOP-QP1RD1B>
2025-02-18 20:35:24 -0300

    Update Classe Usuario

---
+++ To-Do-List-project/src/main/java/br/com/toDoList/model/Pessoa.java
@@ -0,0 +1,11 @@
+package br.com.toDoList.model;
+
+public class Pessoa {
+
+	private String nome;
+	private String genero;
+	private String username;
+	private String telefone;
+	private String endereco;
+	
+}
--- To-Do-List-project/src/main/java/br/com/toDoList/model/Usuario.java
+++ To-Do-List-project/src/main/java/br/com/toDoList/model/Usuario.java
@@ -1,5 +1,96 @@
 package br.com.toDoList.model;
 
-public class Usuario {
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1 )
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+	private Long id;
+	private String nome;
+	private String genero;
+	private String email;
+	private String senha;
+	private String username;
+	private String telefone;
+	private String endereco;
+	private boolean ativo;
+	private LocalDateTime dataCadastro;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	public LocalDateTime getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(LocalDateTime dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+	
+	
 }
