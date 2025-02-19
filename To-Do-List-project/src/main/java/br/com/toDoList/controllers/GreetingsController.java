38380c340f53ae89f65d5dd6a31d12ea89e65a30
jhona <jhona@DESKTOP-QP1RD1B>
2025-02-18 21:31:31 -0300

    Implementado controllers e Repository

--- To-Do-List-project/src/main/java/br/com/toDoList/controllers/GreetingsController.java
+++ To-Do-List-project/src/main/java/br/com/toDoList/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.toDoList.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,20 +8,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.toDoList.model.Usuario;
+import br.com.toDoList.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController
 public class GreetingsController {
+	
+	
     /**
      *
      * @param name the name to greet
      * @return greeting text
      */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/consultarnome/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
+        return "Hello World" + name + "!";
     }
+    
+    /*@RequestMapping(value = "/olamundo/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	
+    	usuarioRepository.save(usuario);
+    	
+    	return "Ola mundo" + nome;
+    }*/
 }
