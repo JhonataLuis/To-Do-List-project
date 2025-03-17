a2cb06cb90298afeee5878ff6e5405b2d504597f
Jhonata <jhonatafontesluis23@gmail.com>
2025-03-16 21:24:51 -0300

    Adicionado camada de segurança simples com Spring Boot Security JWT
    Signed-off-by: Jhonata <jhonatafontesluis23@gmail.com>

---
+++ src/main/java/br/com/toDoList/model/Tarefas.java
@@ -0,0 +1,92 @@
+package br.com.toDoList.model;
+/**
+ * @author Jhonata
+ * @version 1.0
+ * @since 18/02/2025
+ */
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+
+
+@Entity
+@SequenceGenerator(name = "seq_tarefa", sequenceName = "seq_tarefa", allocationSize = 1, initialValue = 1)
+public class Tarefas{
+	/**
+	 * Esta classe é utilizada para criar as tabelas no banco de dados juntamente disponibilizar os atributos
+	 * para trazer cada item atraves na conexão dos atributos
+	 * @param titulo
+	 * @param descricao
+	 * @param prioridade
+	 */
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_tarefa")
+	private Long id;
+	
+	private String titulo;
+	private String descricao;
+	//private boolean concluido;//SIM OU NÃO
+	private String prioridade;//ALTA, MÉDIA, BAIXA - SEPARAR POR COR CADA PRIORIDADE
+	
+	
+	/*@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS][.SS][.S]")
+	@DateTimeFormat(pattern = "dd/mm/yyyy")
+	@NotNull(message = "Campos de data são obrigatórios")
+	@Past(message = "Campos de data são obrigatórios")
+	private LocalDateTime dataCriacao;*/
+	
+	//private LocalDateTime dataConclusao;
+	private String categoria;/*TRABALHO, PESSOAL, ESTUDOS*/
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getPrioridade() {
+		return prioridade;
+	}
+	public void setPrioridade(String prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	/*@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}*/
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
