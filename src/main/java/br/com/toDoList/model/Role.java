ea2c74d63a01b62c931d610150f2bdddeebad899
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-28 12:40:34 -0300

    feat: finalizado configurações da classe Roles
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- src/main/java/br/com/toDoList/model/Role.java
+++ src/main/java/br/com/toDoList/model/Role.java
@@ -1,5 +1,54 @@
 package br.com.toDoList.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    public Role(){}
+
+    public Role(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
 }
