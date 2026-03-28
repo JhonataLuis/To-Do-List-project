45feeaeb65c4d95d601f7361ebd353c15bef4c2f
JhonataLuis <jhonatafontesluis23@gmail.com>
2026-03-28 10:56:22 -0300

    feat: adicionado as dependencias no pom para implementar login com JWT no projeto.
    Signed-off-by: JhonataLuis <jhonatafontesluis23@gmail.com>

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                 <div className="col-md-6">
-                    <h5>To-Do List</h5>
+                    <h5>KeePace</h5>
                     <p>Uma forma simples e eficiente de organizar suas tarefas diárias.</p>
                 </div>
                 <div className="col-md-3">
@@ -21,14 +21,14 @@ const Footer = () => {
                 <div className="col-md-3">
                     <h5>Contato</h5>
                     <ul className="list-unstyled">
-                    <li><i className="bi bi-envelope me-2"></i>contato@todolist.com</li>
+                    <li><i className="bi bi-envelope me-2"></i>contato@keepace.com</li>
                     <li><i className="bi bi-telephone me-2"></i>(11) 99999-9999</li>
                     </ul>
                 </div>
                 </div>
                 <hr className="my-4" />
                 <div className="text-center">
-                <p>&copy; 2026 To-Do List. Todos os direitos reservados.</p>
+                <p>&copy; 2026 KeePace. Todos os direitos reservados.</p>
                 </div>
             </div>
         </footer>
